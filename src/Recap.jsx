a36acaecb0076a817403077a663303b926f06ac2
Félix Poullet-Pagès <felix.poullet-pages@cnrs.fr>
2023-10-01 19:20:51 +0200

    copy to clipboard

--- src/Recap.jsx
+++ src/Recap.jsx
@@ -1,40 +1,61 @@
-import { FormControl, FormLabel, Input, Modal, ModalClose, Sheet, Typography } from '@mui/joy'
+import { FormControl, FormLabel, Input, Modal, ModalClose, Sheet, Tooltip, Typography } from '@mui/joy'
+import { useState } from 'react'
 
-export const Recap = ({ composer, work, open, setOpen }) =>
-  composer &&
-  work && (
-    <Modal
-      aria-labelledby="modal-title"
-      aria-describedby="modal-desc"
-      open={open}
-      onClose={() => setOpen(false)}
-      sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-    >
-      <Sheet
-        variant="outlined"
-        sx={{
-          borderRadius: 'md',
-          p: 3,
-          boxShadow: 'lg',
-        }}
+export const Recap = ({ composer, work, open, setOpen }) => {
+  const [isComposerCopied, setIsComposerCopied] = useState(false)
+  const [isWorkCopied, setIsWorkCopied] = useState(false)
+
+  if (composer && work)
+    return (
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
       >
-        <ModalClose variant="plain" sx={{ m: 1 }} />
-        <Typography component="h2" level="h4" textColor="inherit" fontWeight="lg" mb={1}>
-          This is your <code>{'<MeiHead/>'}</code>
-        </Typography>
-        <FormControl sx={{ paddingY: 2 }}>
-          <FormLabel>
-            Identifiant du compositeur <b>{composer.label}</b>
-          </FormLabel>
-          <Input readOnly value={composer.composer.split('#about').shift()}></Input>
-        </FormControl>
+        <Sheet
+          variant="outlined"
+          sx={{
+            borderRadius: 'md',
+            p: 3,
+            boxShadow: 'lg',
+          }}
+        >
+          <ModalClose variant="plain" sx={{ m: 1 }} />
+          <Typography component="h2" level="h4" textColor="inherit" fontWeight="lg" mb={1}>
+            This is your <code>{'<MeiHead/>'}</code>
+          </Typography>
+          <FormControl sx={{ paddingY: 2 }}>
+            <FormLabel>
+              Identifiant du compositeur <b>{composer.label}</b>
+            </FormLabel>
+            <Tooltip title={isComposerCopied ? 'Copied to clipboard !' : 'Copy to clipboard'}>
+              <Input
+                readOnly
+                onClick={() =>
+                  setIsComposerCopied(true) && navigator.clipboard.writeText(composer.composer.split('#about').shift())
+                }
+                value={composer.composer.split('#about').shift()}
+              ></Input>
+            </Tooltip>
+          </FormControl>
 
-        <FormControl>
-          <FormLabel>
-            Identifiant de l'œuvre <b>{work.label}</b>
-          </FormLabel>
-          <Input readOnly value={work.work.split('#about').shift()}></Input>
-        </FormControl>
-      </Sheet>
-    </Modal>
-  )
+          <FormControl>
+            <FormLabel>
+              Identifiant de l'œuvre <b>{work.label}</b>
+            </FormLabel>
+            <Tooltip title={isWorkCopied ? 'Copied to clipboard !' : 'Copy to clipboard'}>
+              <Input
+                readOnly
+                onClick={() =>
+                  setIsWorkCopied(true) && navigator.clipboard.writeText(work.work.split('#about').shift())
+                }
+                value={work.work.split('#about').shift()}
+              ></Input>
+            </Tooltip>
+          </FormControl>
+        </Sheet>
+      </Modal>
+    )
+}
